34984f002621e8ee94246921276c38a15ed54997
sooyeon <qkrtndus130@gmail.com>
2021-09-13 23:45:39 +0900

    jsoup으로 사이트 이미지 파싱

---
+++ app/src/main/java/com/yoni/imagecrawlerapp/ImageUrlParser.kt
@@ -0,0 +1,21 @@
+package com.yoni.imagecrawlerapp
+
+import android.util.Log
+import org.jsoup.Jsoup
+import java.util.ArrayList
+
+object ImageUrlParser {
+    private val urlList :ArrayList<String> = ArrayList()
+
+    fun parseImageUrl(){
+        val doc = Jsoup.connect("https://gettyimagesgallery.com/collection/sasha/").get()
+        Log.d("TTT","connection setup and parsing start")
+        val elements = doc.select("div.item-wrapper img.jq-lazy")
+        for( e in elements){
+            val url : String = e.attr("data-src")
+            urlList.add(url)
+        }
+        Log.d("TTT","parsing done")
+    }
+
+}
