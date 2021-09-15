645203e3ef13e293a4ab12693771de7158504c3c
sooyeon <qkrtndus130@gmail.com>
2021-09-15 16:42:05 +0900

    성능비교 위해 glide 추가

--- app/src/main/java/com/yoni/imagecrawlerapp/ImageAdapter.kt
+++ app/src/main/java/com/yoni/imagecrawlerapp/ImageAdapter.kt
@@ -7,12 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.util.*
+
 
 class ImageAdapter(private val context: Context, private val imgUrlList: ArrayList<String>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -30,6 +36,11 @@ class ImageAdapter(private val context: Context, private val imgUrlList: ArrayLi
         }
     }
 
+//    //안하는게 좋음. 재활용성의 장점을 못씀..
+//    override fun getItemViewType(position: Int): Int {
+//        return position
+//    }
+
     override fun getItemCount(): Int {
         return imgUrlList.size;
     }
@@ -39,11 +50,21 @@ class ImageAdapter(private val context: Context, private val imgUrlList: ArrayLi
         private var bitmap: Bitmap? = null
 
         fun bind(url: String) {
+
+            //성능 비교용 glide
+//            Glide.with(context).load(url)
+//                .override(600,600)
+//                .skipMemoryCache(true)
+//                .diskCacheStrategy(DiskCacheStrategy.NONE)
+//                .into(img)
+
             CoroutineScope(Dispatchers.Main).launch {
                 bitmap = withContext(Dispatchers.IO) {
                     ImageLoader.loadImage(url)
                 }
+
                 img.setImageBitmap(bitmap)
+                //bitmap이 남아있어서 이상해보임
             }
         }
     }
